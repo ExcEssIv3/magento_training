3d125137d3f256e187e4f2aba3ec8d6d6ae42854
Seth Rowland <sethr@dckap.com>
2022-05-23 12:25:38 -0500

    changed how redirects work after insert/edit/delete

--- code/Training/Bookings/Controller/Index/Delete.php
+++ code/Training/Bookings/Controller/Index/Delete.php
@@ -6,32 +6,31 @@ use Magento\Framework\App\Action\HttpDeleteActionInterface;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 
 class Delete implements HttpGetActionInterface {
-    protected $_pageFactory;
+    protected $_redirectFactory;
     protected $_request;
     protected $_bookingResource;
     protected $_bookingFactory;
 
     public function __construct(
-        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory,
         \Magento\Framework\App\Request\Http $request,
         \Training\Bookings\Model\ResourceModel\Booking $bookingResource,
         \Training\Bookings\Model\BookingFactory $bookingFactory
     ) {
-        $this->_pageFactory = $pageFactory;
+        $this->_redirectFactory = $redirectFactory;
         $this->_request = $request;
         $this->_bookingResource = $bookingResource;
         $this->_bookingFactory = $bookingFactory;
     }
 
     public function execute() {
-        $resultPage = $this->_pageFactory->create();
+        $redirect = $this->_redirectFactory->create();
         $id = $this->_request->getParam('id');
         $postData = $this->_bookingFactory->create();
         $result = $postData->setId($id);
         $this->_bookingResource->delete($result);
-        $resultPage->getConfig()->getTitle()->prepend(__('Booking Deleted!'));
-
-        return $resultPage;
+        $redirect->setPath("*/*/index", ['save'=>1]);
+        return $redirect;
     }
 }
 
--- code/Training/Bookings/Controller/Index/Save.php
+++ code/Training/Bookings/Controller/Index/Save.php
@@ -6,39 +6,37 @@ use Magento\Framework\App\Action\HttpPostActionInterface;
 
 class Save implements HttpPostActionInterface {
     protected $_request;
-    protected $_pageFactory;
+    protected $_redirectFactory;
     protected $_bookingResource;
     protected $_bookingFactory;
 
     public function __construct(
         \Magento\Framework\App\RequestInterface $request,
-        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory,
         \Training\Bookings\Model\ResourceModel\Booking $bookingResource,
         \Training\Bookings\Model\BookingFactory $bookingFactory
     ) {
         $this->_request = $request;
-        $this->_pageFactory = $pageFactory;
+        $this->_redirectFactory = $redirectFactory;
         $this->_bookingResource = $bookingResource;
         $this->_bookingFactory = $bookingFactory;
     }
 
     public function execute() {
-        $resultPage = $this->_pageFactory->create();
+        $redirect = $this->_redirectFactory->create();
         $input = $this->_request->getParams();
         $postData = $this->_bookingFactory->create();
         if ($input['id'] != "-1") {
             $this->_bookingResource->load($postData, $input['id']);
             $postData->addData($input);
             $this->_bookingResource->save($postData);
-            $resultPage->getConfig()->getTitle()->prepend(__('Booking Edited!'));
         } else {
             unset($input['id']);
             $postData->setData($input);
             $this->_bookingResource->save($postData);
-            $resultPage->getConfig()->getTitle()->prepend(__('Booking Created!'));
         }
-
-        return $resultPage;
+        $redirect->setPath('*/*/index', ['save'=>0]);
+        return $redirect;
     }
 }
 
